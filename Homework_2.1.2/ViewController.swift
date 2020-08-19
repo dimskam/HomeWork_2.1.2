f2741383d0085936d0b8a1b8199470a4a34510b5
KamenkovDS <dimskam@gmail.com>
2020-08-19 11:09:34 +0300

    Initial Commit

---
+++ Homework_2.1.2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Homework_2.1.2
+//
+//  Created by Dmitriy Kamenkov on 19.08.2020.
+//  Copyright © 2020 Dmitriy Kamenkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
